e34cb7271edd5ef193b2cb922f14f720d195709f
snacksncode <snacksncode@gmail.com>
2021-10-01 16:54:57 +0200

    feat: removed "api" and started to actually use Next.js pre-rendring powers

--- pages/api/data/getAllIds.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import allData from "./files";
-
-export default async function getAllData(_req: NextApiRequest, res: NextApiResponse) {
-  const ids = allData.map((d) => d.id);
-  res.status(200).json({ ids });
-}
--- pages/api/data/index.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import allData from "./files";
-
-export default async function getAllData(_req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ data: allData });
-}
