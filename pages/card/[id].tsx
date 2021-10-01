08e9ed6f8073eb351b07fcac157b64f5e84df2bc
snacksncode <snacksncode@gmail.com>
2021-10-01 13:46:53 +0200

    feat: converted index page to use new Viewer component and api

--- pages/card/[id].tsx
+++ pages/card/[id].tsx
@@ -1,13 +1,16 @@
+import Viewer from "@components/Viewer";
 import { NextPage } from "next";
-import Link from "next/link";
+import { useRouter } from "next/dist/client/router";
 import React, { useEffect, useState } from "react";
 
 const API_PATH = "/api/data";
 
 const Home: NextPage = () => {
+  const { id } = useRouter().query;
   const [data, setData] = useState<null | any[]>(null);
   useEffect(() => {
-    fetch(API_PATH)
+    if (!id) return;
+    fetch(`${API_PATH}/${id}`)
       .then((res) => {
         if (!res.ok) throw new Error("Data fetching failed");
         return res.json();
@@ -15,19 +18,9 @@ const Home: NextPage = () => {
       .then((d) => {
         setData(d.data);
       });
-  }, []);
+  }, [id]);
   if (!data) return <p>Loading...</p>;
-  return (
-    <p>
-      {data.map((d: any) => {
-        return (
-          <Link key={d.id} href={`card/${d.id}`}>
-            <a style={{ display: "block" }}>Go to: {d.id}</a>
-          </Link>
-        );
-      })}
-    </p>
-  );
+  return <Viewer data={data} />;
 };
 
 export default Home;
