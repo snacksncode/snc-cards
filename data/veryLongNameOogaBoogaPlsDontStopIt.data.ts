54d4d431f6468f301817b853ec56fceafa0421b3
snacksncode <snacksncode@gmail.com>
2021-10-06 13:04:04 +0200

    feat: added beta animations for filtering

---
+++ data/veryLongNameOogaBoogaPlsDontStopIt.data.ts
@@ -0,0 +1,23 @@
+const wordsData = [
+  {
+    "question": "b",
+    "answer": "a"
+  },
+  {
+    "question": "d",
+    "answer": "c"
+  },
+  {
+    "question": "f",
+    "answer": "e"
+  }
+]
+
+const exported = {
+  id: "veryLongNameOogaBoogaPlsDontStopIt",
+  data: wordsData,
+  lang: "lang",
+  tags: ["tag"],
+};
+
+export default exported;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import { NextPage } from "next";
 import Link from "next/link";
 import Fuse from "fuse.js";
-import React, { useRef, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import { getData } from "@data/exporter";
 import styles from "@styles/Home.module.scss";
 import { AnimatePresence, motion } from "framer-motion";
@@ -19,7 +19,6 @@ const Home: NextPage<Props> = ({ dataObject }) => {
   const [filteredData, setFilteredData] = useState(dataObject);
   const fuse = useRef(
     new Fuse(dataObject, {
-      shouldSort: false,
       keys: [
         {
           name: "id",
@@ -40,37 +39,59 @@ const Home: NextPage<Props> = ({ dataObject }) => {
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
-    filter(e.target.value);
   };
 
-  const filter = (filterString: string) => {
-    if (!filterString) {
-      setFilteredData(dataObject);
-      return;
-    }
-    const data = fuse.current.search(filterString);
-    setFilteredData(data as any);
-  };
+  const filter = useCallback(
+    (filterString: string) => {
+      if (!filterString) {
+        setFilteredData(dataObject);
+        return;
+      }
+      const data = fuse.current.search(filterString);
+      setFilteredData(data as any);
+    },
+    [dataObject]
+  );
+
+  useEffect(() => {
+    const TIMEOUT_MS = inputValue.length === 0 ? 0 : 250;
+    const timeoutId = window.setTimeout(() => {
+      filter(inputValue);
+    }, TIMEOUT_MS);
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [inputValue, filter]);
 
   return (
     <main className={styles.wrapper}>
       <h1>Select the topic that you want to revise</h1>
       <label htmlFor="search">Look for topics</label>
       <input id="search" type="text" value={inputValue} onChange={handleInputChange} />
-      <div className={styles.grid}>
+      <motion.div className={styles.grid}>
         <AnimatePresence>
           {filteredData.map((d: any) => {
             const entryData = d.item ? d.item : d;
             return (
-              <motion.div key={entryData.id} initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+              <motion.div
+                layout
+                layoutId={entryData.id}
+                key={entryData.id}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                className={styles.entry}
+                exit={{ opacity: 0 }}
+              >
                 <Link key={entryData.id} href={`card/${entryData.id}`}>
-                  <a style={{ display: "block" }}>Go to: {entryData.id}</a>
+                  <a className={styles.entry__title} style={{ display: "block" }}>
+                    {entryData.id}
+                  </a>
                 </Link>
               </motion.div>
             );
           })}
         </AnimatePresence>
-      </div>
+      </motion.div>
     </main>
   );
 };
