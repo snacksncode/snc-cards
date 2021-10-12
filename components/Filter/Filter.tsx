f312981f4146a5043ed065e90241a2dacd00d83c
snacksncode <snacksncode@gmail.com>
2021-10-12 22:05:20 +0200

    feat+refactor: added Filter component and made it keyboard-focusable

---
+++ components/Filter/Filter.tsx
@@ -0,0 +1,49 @@
+import { motion } from "framer-motion";
+import { ChangeEventHandler, useEffect, useRef } from "react";
+import styles from "./Filter.module.scss";
+
+interface Props {
+  value: string;
+  onChangeHandler: ChangeEventHandler<HTMLInputElement>;
+}
+
+const Filter = ({ value, onChangeHandler }: Props) => {
+  const inputRef = useRef<null | HTMLInputElement>(null);
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      if (e.ctrlKey && e.key === "k") {
+        e.preventDefault(); // prevent browser shortcut
+        inputRef.current?.focus();
+      }
+    };
+    window.addEventListener("keydown", handler);
+    return () => {
+      window.removeEventListener("keydown", handler);
+    };
+  }, []);
+  return (
+    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: 0.1 } }} className={styles.field}>
+      <label htmlFor="search">Filter topics</label>
+      <div className={styles.input__wrapper}>
+        <input
+          ref={inputRef}
+          id="search"
+          type="text"
+          placeholder="...by name & tags"
+          value={value}
+          onChange={onChangeHandler}
+        />
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: value.length === 0 ? 1 : 0 }}
+          className={styles.keyboard__indicator}
+        >
+          <div className={styles.key}>Ctrl</div>
+          <div className={styles.key}>K</div>
+        </motion.div>
+      </div>
+    </motion.div>
+  );
+};
+
+export default Filter;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import { NextPage } from "next";
-import React, { useEffect, useState } from "react";
+import React, { ChangeEventHandler, useEffect, useState } from "react";
 import { getData } from "@data/exporter";
 import styles from "@styles/Home.module.scss";
 import ListEntries from "@components/ListEntries";
 import { motion } from "framer-motion";
+import Filter from "@components/Filter";
 
 // const ITEMS_PER_PAGE = 4;
 
@@ -15,7 +16,7 @@ const Home: NextPage<Props> = ({ dataArray }) => {
   const [inputValue, setInputValue] = useState("");
   const [filterString, setFilterString] = useState<string | null>(null);
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleInputChange: ChangeEventHandler<HTMLInputElement> = (e) => {
     setInputValue(e.target.value);
   };
 
@@ -34,20 +35,7 @@ const Home: NextPage<Props> = ({ dataArray }) => {
       <motion.h1 initial={{ opacity: 0, y: -10 }} animate={{ opacity: 1, y: 0 }} className={styles.heading}>
         Hi! Select the topic that you want to revise
       </motion.h1>
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1, transition: { delay: 0.1 } }}
-        className={styles.field}
-      >
-        <label htmlFor="search">Filter topics</label>
-        <input
-          id="search"
-          type="text"
-          placeholder="...by name & tags"
-          value={inputValue}
-          onChange={handleInputChange}
-        />
-      </motion.div>
+      <Filter value={inputValue} onChangeHandler={handleInputChange} />
       <ListEntries filterString={filterString} dataArray={dataArray} />
     </main>
   );
