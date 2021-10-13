637114704f2b34d46e4582da64dc8243a7088d46
snacksncode <snacksncode@gmail.com>
2021-10-13 12:12:03 +0200

    data: forgot to uncomment

--- data/german.data.ts
+++ data/german.data.ts
@@ -1,123 +1,119 @@
 const wordsData = [
-  // {
-  //   question: "Pokój jednoosobowy",
-  //   answer: "Einzelzimmer",
-  // },
-  // {
-  //   question: "Pokój dwuosobowy",
-  //   answer: "Doppelzimmer",
-  // },
-  // {
-  //   question: "Łącze internetowe",
-  //   answer: "Internet Anschluss",
-  // },
-  // {
-  //   question: "Miejsce kampingowe",
-  //   answer: "Campingplatz",
-  // },
-  // {
-  //   question: "Schronisko młodzieżowe",
-  //   answer: "Jugendherberge",
-  // },
-  // {
-  //   question: "Zwierzeta są dozwolone",
-  //   answer: "Haustiere erlaubt",
-  // },
-  // {
-  //   question: "Telefon w pokoju",
-  //   answer: "Telefon im Zimmer",
-  // },
-  // {
-  //   question: "TV w pokoju",
-  //   answer: "Fernseher im Zimmer",
-  // },
-  // {
-  //   question: "Miejsce parkingowe",
-  //   answer: "Parkplatz",
-  // },
-  // {
-  //   question: "Pokój z prysznicem",
-  //   answer: "Zimmer mit Dusche",
-  // },
-  // {
-  //   question: "Liczyć",
-  //   answer: "rechnen",
-  // },
-  // {
-  //   question: "Zameldować się (a...)",
-  //   answer: "anmelden",
-  // },
-  // {
-  //   question: "Imię / Nazwisko (?)",
-  //   answer: "Namen",
-  // },
-  // {
-  //   question: "Pokój",
-  //   answer: "das Zimmer",
-  // },
-  // {
-  //   question: "Zameldowanie",
-  //   answer: "Anmeldung",
-  // },
-  // {
-  //   question: "Rezerwacja",
-  //   answer: "Reservierung",
-  // },
-  // {
-  //   question: "Nocleg",
-  //   answer: "Übernachtung",
-  // },
-  // {
-  //   question: "wypełniać",
-  //   answer: "ausfüllen",
-  // },
-  // {
-  //   question: "Zameldować się (e...)",
-  //   answer: "einchecken",
-  // },
-  // {
-  //   question: "wyjechać (czas. od podróż)",
-  //   answer: "abreisen",
-  // },
-  // {
-  //   question: "Wymeldować się",
-  //   answer: "auschecken",
-  // },
-  // {
-  //   question: "Zwiedzać",
-  //   answer: "besichtigen",
-  // },
-  // {
-  //   question: '"Namiotować"',
-  //   answer: "zelten",
-  // },
-  // {
-  //   question: "Zarezerwować",
-  //   answer: "reservieren",
-  // },
-  // {
-  //   question: "zostać / pozostać",
-  //   answer: "bleiben",
-  // },
-  // {
-  //   question: "podróż(e)",
-  //   answer: "Reise(n)",
-  // },
-  // {
-  //   question: "spędzać",
-  //   answer: "verbringen",
-  // },
-  // {
-  //   question: "życzyć",
-  //   answer: "wünschen",
-  // },
-  // {
-  //   question: "nocować / przenocować",
-  //   answer: "übernachten",
-  // },
   {
-    question: "Test",
-    answer: "Helloß",
+    question: "Pokój jednoosobowy",
+    answer: "Einzelzimmer",
+  },
+  {
+    question: "Pokój dwuosobowy",
+    answer: "Doppelzimmer",
+  },
+  {
+    question: "Łącze internetowe",
+    answer: "Internet Anschluss",
+  },
+  {
+    question: "Miejsce kampingowe",
+    answer: "Campingplatz",
+  },
+  {
+    question: "Schronisko młodzieżowe",
+    answer: "Jugendherberge",
+  },
+  {
+    question: "Zwierzeta są dozwolone",
+    answer: "Haustiere erlaubt",
+  },
+  {
+    question: "Telefon w pokoju",
+    answer: "Telefon im Zimmer",
+  },
+  {
+    question: "TV w pokoju",
+    answer: "Fernseher im Zimmer",
+  },
+  {
+    question: "Miejsce parkingowe",
+    answer: "Parkplatz",
+  },
+  {
+    question: "Pokój z prysznicem",
+    answer: "Zimmer mit Dusche",
+  },
+  {
+    question: "Liczyć",
+    answer: "rechnen",
+  },
+  {
+    question: "Zameldować się (a...)",
+    answer: "anmelden",
+  },
+  {
+    question: "Imię / Nazwisko (?)",
+    answer: "Namen",
+  },
+  {
+    question: "Pokój",
+    answer: "das Zimmer",
+  },
+  {
+    question: "Zameldowanie",
+    answer: "Anmeldung",
+  },
+  {
+    question: "Rezerwacja",
+    answer: "Reservierung",
+  },
+  {
+    question: "Nocleg",
+    answer: "Übernachtung",
+  },
+  {
+    question: "wypełniać",
+    answer: "ausfüllen",
+  },
+  {
+    question: "Zameldować się (e...)",
+    answer: "einchecken",
+  },
+  {
+    question: "wyjechać (czas. od podróż)",
+    answer: "abreisen",
+  },
+  {
+    question: "Wymeldować się",
+    answer: "auschecken",
+  },
+  {
+    question: "Zwiedzać",
+    answer: "besichtigen",
+  },
+  {
+    question: '"Namiotować"',
+    answer: "zelten",
+  },
+  {
+    question: "Zarezerwować",
+    answer: "reservieren",
+  },
+  {
+    question: "zostać / pozostać",
+    answer: "bleiben",
+  },
+  {
+    question: "podróż(e)",
+    answer: "Reise(n)",
+  },
+  {
+    question: "spędzać",
+    answer: "verbringen",
+  },
+  {
+    question: "życzyć",
+    answer: "wünschen",
+  },
+  {
+    question: "nocować / przenocować",
+    answer: "übernachten",
   },
 ];
 
