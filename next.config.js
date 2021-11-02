07c46d6e710be81b2dd755ca6d3f2cb10ef76f90
snacksncode <snacksncode@gmail.com>
2021-11-02 20:20:14 +0100

    config: removed custom webpack config from next.js', reason: no longer using raw-loader

--- next.config.js
+++ next.config.js
@@ -2,11 +2,4 @@
 module.exports = {
   swcMinify: true,
   reactStrictMode: true,
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.txt$/,
-      use: "raw-loader",
-    });
-    return config;
-  },
 };
