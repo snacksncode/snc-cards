792fbd70fa074b2ee17a3fbf9d6dd5891d235143
snacksncode <snacksncode@gmail.com>
2022-11-14 22:08:50 +0100

    packages: fixed react types problems during build

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,7 @@
 import "@styles/globals.scss";
 import type { AppProps } from "next/app";
-// import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  // useEffect(() => {
-  //   document.body.classList.add("dark");
-  // });
   return <Component {...pageProps} />;
 }
 export default MyApp;
